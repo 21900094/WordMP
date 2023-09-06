71141a14d08559fcff8c161a0e393a07d392b7c8
21900094 <21900094@handong.ac.kr>
2023-09-07 01:24:29 +0900

    first commit

--- src/main/java/org/example/ICRUD.java
+++ src/main/java/org/example/ICRUD.java
@@ -1,2 +1,8 @@
-package org.example;public interface ICRUD {
+package org.example;
+
+public interface ICRUD {
+    public Object add();
+    public int updateObject(Object obj);
+    public int deleteObject(Object obj);
+    public void selectObject(int id);
 }
