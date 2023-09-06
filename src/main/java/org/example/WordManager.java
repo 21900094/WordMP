556916ed70475ca15703e11c3ab620025262bb92
21900094 <21900094@handong.ac.kr>
2023-09-07 01:43:46 +0900

    img upload

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -31,7 +31,8 @@ public class WordManager {
         while(true) {
             int menu=selectMenu();
             if(menu==0) {
-
+                System.out.println("프로그램 종료! 다음에 만나요~");
+                break;
             }
             else if(menu==4) {
                 //create
